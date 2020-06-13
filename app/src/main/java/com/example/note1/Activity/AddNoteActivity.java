92ad3f86c6eace9e252b2478f9af1d227fe74c96
HungPain69 <hungpain69@gmail.com>
2020-06-13 21:35:33 +0700

    Change format date time and update time after modify each note

--- app/src/main/java/com/example/note1/Activity/AddNoteActivity.java
+++ app/src/main/java/com/example/note1/Activity/AddNoteActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +14,10 @@ import com.example.note1.Model.NoteObj;
 import com.example.note1.R;
 import com.example.note1.Sqlite.DBHelper;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class AddNoteActivity extends AppCompatActivity {
 
     EditText addTitle;
@@ -20,7 +25,7 @@ public class AddNoteActivity extends AppCompatActivity {
     EditText addDateTime;
     NoteObj note;
     Button btnSave;
-
+    String getTimeNow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,15 +33,19 @@ public class AddNoteActivity extends AppCompatActivity {
 
         this.addTitle = (EditText)findViewById(R.id.addTitle);
         this.addDetail = (EditText)findViewById(R.id.addDetail);
-        this.addDateTime = (EditText)findViewById(R.id.addDateTime);
+//        this.addDateTime = (EditText)findViewById(R.id.addDateTime);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm dd 'thg' MM', 'yyyy");
+        Date now = Calendar.getInstance().getTime();
+        getTimeNow = dateFormat.format(now);
+        Log.d("datetime",dateFormat.format(now)+"");
     }
     //    btnSave =
     public void onClickSave(View view)  {
         DBHelper db = new DBHelper(this);
         String title = this.addTitle.getText().toString();
         String content = this.addDetail.getText().toString();
-        String dateTime = this.addDateTime.getText().toString();
-        this.note = new NoteObj(title,content,dateTime);
+//        String dateTime = this.addDateTime.getText().toString();
+        this.note = new NoteObj(title,content,getTimeNow);
 
         DBHelper dbHelper = new DBHelper(this);
         dbHelper.addNote(this.note);
--- app/src/main/java/com/example/note1/Activity/ShowNEditActivity.java
+++ app/src/main/java/com/example/note1/Activity/ShowNEditActivity.java
@@ -7,14 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.note1.MainActivity;
 import com.example.note1.Model.NoteObj;
 import com.example.note1.R;
 import com.example.note1.Sqlite.DBHelper;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class ShowNEditActivity extends AppCompatActivity {
-    EditText editTextTitle, editTextContent, editTextDateTime;
+    EditText editTextTitle, editTextContent;
+    TextView textViewDateTime;
+    String mOldTitle, mOldDetail;
 
     NoteObj note;
     Button buttonSave;
@@ -24,20 +31,32 @@ public class ShowNEditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show_n_edit);
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextContent = findViewById(R.id.edit_text_content);
-        editTextDateTime = findViewById(R.id.edit_text_dateTime);
+        textViewDateTime = findViewById(R.id.tv_dateTime);
         Intent intent = getIntent();
         this.note = (NoteObj) intent.getSerializableExtra("note");
-        editTextTitle.setText(note.getTitle());
-        editTextContent.setText(note.getDetail());
-        editTextDateTime.setText(note.getDateTime());
+
+         mOldTitle = note.getTitle();
+        editTextTitle.setText(mOldTitle);
+         mOldDetail = note.getDetail();
+        editTextContent.setText(mOldDetail);
+        textViewDateTime.setText("Đã chỉnh sửa "+note.getDateTime());
 
 
     }
     public void onClickSave(View view){
         DBHelper db = new DBHelper(this);
-        this.note.setTitle(editTextTitle.getText().toString());
-        this.note.setDetail(editTextContent.getText().toString());
-        this.note.setDateTime(editTextDateTime.getText().toString());
+        String newTiltle = editTextTitle.getText().toString();
+        this.note.setTitle(newTiltle);
+        String newDetail = editTextContent.getText().toString();
+        this.note.setDetail(newDetail);
+        if(!mOldTitle.equals(newTiltle) || !mOldDetail.equals(newDetail)) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm dd 'thg' MM', 'yyyy");
+            Date now = Calendar.getInstance().getTime();
+            String getTimeNow = dateFormat.format(now);
+            this.note.setDateTime(getTimeNow);
+        }else{
+            this.note.setDateTime(textViewDateTime.getText().toString());
+        }
 
         db.updateNote(note);
         Intent inten= new Intent(ShowNEditActivity.this, MainActivity.class);
